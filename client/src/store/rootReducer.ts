796483555952ca29abb0fe860baaefabb56d769b
Francisco <javiermartincuenca@gmail.com>
2019-12-08 17:03:06 +0100

    initial setup

---
+++ client/src/store/rootReducer.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { IGlobalState } from '../interfaces/states';
+
+const rootReducer = combineReducers<IGlobalState>({});
+
+export default rootReducer;
