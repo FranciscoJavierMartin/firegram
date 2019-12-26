f64d7abd92f27d55e2667825ef674b0efc96db99
Francisco <javiermartincuenca@gmail.com>
2019-12-26 22:43:49 +0100

    Fixed image URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header';
 import CameraPage from './pages/camera/CameraPage';
-//import Camera from './utils/device/Camera';
-
-
-function addDataTest(data: string) {
-  return process.env.NODE_ENV === 'test' ? data : undefined; 
-}
 
 const App: React.FC = () => {
   console.log(process.env.NODE_ENV);
   return (
-    <div className="App" data-test='component-app' data-myo={addDataTest('home')}>
+    <div className="App" data-test='component-app'>
       <Header/>
      <CameraPage/>
     </div>
--- client/src/components/camera/Camera.tsx
+++ client/src/components/camera/Camera.tsx
@@ -11,51 +11,60 @@ const Camera = (props: ICameraProps) => {
   const videoRef = useRef<any>();
   const canvasRef = useRef<any>();
 
-  const mediaStream = useUserMedia({ audio: false, video: { width: 300, height: 300}});
+  const mediaStream = useUserMedia({
+    audio: false,
+    video: { width: 300, height: 300 }
+  });
 
-  if (mediaStream && videoRef.current && !videoRef.current.srcObject){
+  if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
     videoRef.current.srcObject = mediaStream;
   }
 
   // Stop the video stream when component is unmount
   useEffect(() => {
     return () => {
-      if(mediaStream){
+      if (mediaStream) {
         mediaStream.getTracks()[0].stop();
       }
-    }
+    };
   }, [mediaStream]);
 
-  const onCapture= async (blob: any) => {
-    const imageUrl = await storage.ref('images/prueba1.png').put(blob).snapshot.ref.getDownloadURL();
+  const onCapture = async (blob: any) => {
+    const imageUrl = await (
+      await storage.ref('images/prueba1.png').put(blob)
+    ).ref.getDownloadURL();
     props.setImageUrl(imageUrl);
-    console.log(imageUrl);
-    //console.log('URL', myPromise);
-    if(mediaStream){
+    if (mediaStream) {
       mediaStream.getTracks()[0].stop();
     }
   };
 
-  return(
+  return (
     <div>
-      <video ref={videoRef} autoPlay muted/>
-      <canvas ref={canvasRef} width={300} height={300}/>
-      <button onClick={() => {
-        if(canvasRef){
-          const context = canvasRef.current.getContext('2d');
-          // This is for rotate the photo
-          context.translate(300,0);
-          context.scale(-1,1);
+      <video ref={videoRef} autoPlay muted />
+      <canvas ref={canvasRef} width={300} height={300} />
+      <button
+        onClick={() => {
+          if (canvasRef) {
+            const context = canvasRef.current.getContext('2d');
+            // This is for rotate the photo
+            context.translate(300, 0);
+            context.scale(-1, 1);
 
-          context.drawImage(videoRef.current, 0,0, 300, 300);          
-          canvasRef.current.toBlob((blob: any) => onCapture(blob), "image/jpeg", 1);
-          context.clearRect(0,0, 300, 300);
-        }
-      }}>
+            context.drawImage(videoRef.current, 0, 0, 300, 300);
+            canvasRef.current.toBlob(
+              (blob: any) => onCapture(blob),
+              'image/jpeg',
+              1
+            );
+            context.clearRect(0, 0, 300, 300);
+          }
+        }}
+      >
         Take photo
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default Camera;
